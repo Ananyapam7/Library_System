789a342a49cb84080ab384d9ab54922bc9059a6a
Ananyapam De <ananyapam7@gmail.com>
2020-10-25 16:48:55 +0530

    commit

--- library_system.c
+++ library_system.c
@@ -39,16 +39,17 @@ struct book a;
 
 void mainmenu_user(void);
 void mainmenu_admin(void);
+
 void add(void);
 void delete(void);
 void edit(void);
-void search(void);
-void issue(void);
-void view(void);
+
+
+void issue_book(void);
+void return_book(void);
+
 void closeapplication(void);
-int  getdata();
 void Password();
-void issuerecord();
 
 
 void main()
@@ -100,16 +101,10 @@ printf("      1. Add Books");
 gotoxy(20,7);
 printf("      2. Delete books");
 gotoxy(20,9);
-printf("      3. Search Books");
+printf("      3. Edit Book's Record");
 gotoxy(20,11);
-printf("      4. Issue Books");
+printf("      4. Close Application");
 gotoxy(20,13);
-printf("      5. View Book list");
-gotoxy(20,15);
-printf("      6. Edit Book's Record");
-gotoxy(20,17);
-printf("      7. Close Application");
-gotoxy(20,19);
 printf("Enter your choice: \n");
 
 scanf("%d", choice);
@@ -125,13 +120,12 @@ int i;
 gotoxy(40,3);
 printf(" MAIN MENU FOR USER");
 gotoxy(20,5);
-printf("      1. Search Books");
+printf("      1. Issue Books");
 gotoxy(20,7);
-printf("      2. Issue Books");
+printf("      2. Return Books");
 gotoxy(20,9);
-printf("      3. View Book list");
+printf("      3. Close Application");
 gotoxy(20,11);
-printf("      4. Close Application");
-gotoxy(20,13);
-printf("Enter your choice: ");
+printf("Enter your choice: \n");
+
 }
