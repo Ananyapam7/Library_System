70327bf7756e7fadff15a59ac924cca0b8947f4d
Ananyapam De <ananyapam7@gmail.com>
2020-10-26 19:59:44 +0530

    commit

--- library_system.c
+++ library_system.c
@@ -1,51 +1,58 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<string.h>                  
+#include<string.h>
+#include<stdlib.h>                  
 #include<ctype.h>                   
 #include<time.h>
-
-// void gotoxy(int x,int y)
-// {
-//     printf("%c[%d;%df",0x1B,y,x);
-// }
-
-// struct meroDate
-// {
-//     int mm,dd,yy;
-// };
-
+#define MAX 3
 
 struct book
 {
     int id;
-    // char name;
-    // char Author;
+    char name[20];
+    char author[20];
     int quantity;
-    // char publisher;
-    // struct meroDate issued;
-    // struct meroDate duedate;
+    float marks;
 };
 
 int main()
-{    
-int i;    
-struct book b[5];    
-printf("Enter Records of 5 students");    
-for(i=0;i<5;i++){    
-printf("\nEnter id:");    
-scanf("%d", &b[i].id);    
-// printf("\nEnter Name:"); 
-// fgets(&b[i].name, sizeof(b[i].name), stdin);
-// printf("\nEnter Author:");    
-// fgets(&b[i].Author, sizeof(b[i].Author), stdin);   
-printf("\nEnter Quantity:");    
-scanf("%d", &b[i].quantity);
-// printf("\nEnter Publisher:");    
-// fgets(&b[i].publisher, sizeof(b[i].publisher), stdin);   
+{
+    // int MAX;
+    printf("\t\t\t\t\t\tLibrary Setup\n");
+    printf("\t\tYou need to add some preliminary books to your library to continue further\n\n");
+    printf("How many books do you want to add\n");
+    // scanf("%d", MAX);
+    struct book arr_book[MAX];
+    int i;
+
+    for(i = 0; i < MAX; i++ )
+    {
+        printf("\nEnter details of book %d\n\n", i+1);
+
+        printf("EnterID: ");
+        scanf("%d", &arr_book[i].id);
+
+        printf("Enter Name: ");
+        scanf("%s", arr_book[i].name);
+
+        printf("Enter Author: ");
+        scanf("%s", arr_book[i].author);
+
+        printf("Enter Quantity: ");
+        scanf("%d", &arr_book[i].quantity);
+
+        printf("Enter Price: ");
+        scanf("%f", &arr_book[i].marks);
+    }
+
+    printf("\n");
+
+    printf("ID\tName\tAuthor\tQuantity\tPrice\n");
+
+    for(i = 0; i < MAX; i++ )
+    {
+        printf("%d\t%s\t%s\t%d\t\t%.2f\n",
+        arr_book[i].id,arr_book[i].name,arr_book[i].author, arr_book[i].quantity, arr_book[i].marks);
+    }
+
+    return 0;
 }
-printf("\nBook Information List:");    
-for(i=0;i<5;i++){    
-printf("\nRollno:%d, Quantity:%d",b[i].id,b[i].quantity);    
-}    
-   return 0;    
-}   
